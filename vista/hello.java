ff1c02a834a3c5438fefe587d93b744ded90a807
manrique20 <jsebastianm45@gmail.com>
2024-05-07 19:18:16 -0500

    UI commits

--- vista/hello.java
+++
@@ -1,9 +0,0 @@
-package vista;
-
-public class hello {
-    public class HolaMundo {
-        public static void main (String[] args) {
-          System.out.println("Hola, mundo!");
-        }
-      }
-}
